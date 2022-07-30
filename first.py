864e5ab438a7d527c17a794c4b0dde81a6800baa
Jaesung Liam Lee <109929998+liam-jaesung@users.noreply.github.com>
2022-07-30 09:39:59 +0900

    Update first.py

--- first.py
+++ first.py
@@ -1,5 +1,8 @@
 #this is my first file
 #this is my second modification
 #this is my third modification
+<<<<<<< HEAD
 #this is my forth modification on test-function branch
 
+=======
+>>>>>>> f2d8471 (Update first.py)
